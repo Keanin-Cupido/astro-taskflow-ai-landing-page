e1e5393b9b80a153489cf944b6af509c0ad94509
Keanin Cupido <keanin16@gmail.com>
2024-11-17 20:36:43 +0200

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  integrations: [tailwind()],
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
+});
